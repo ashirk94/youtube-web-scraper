fcc5a4498f6eba60ec27126d0960976cf292044c
Alan Shirk <ashirk94@gmail.com>
2022-11-03 00:50:39 -0700

    Completed channel scraper
    Now writes all video titles to a file, each on a separate line.

--- scraper.py
+++ scraper.py
@@ -1,9 +1,48 @@
 from bs4 import BeautifulSoup
-#from selenium import webdriver
-import requests
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-html = requests.get('https://www.crummy.com/software/BeautifulSoup/bs4/doc/#calling-a-tag-is-like-calling-find-all').text
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
+# freeCodeCamp videos
+driver.get(url = 'https://www.youtube.com/c/Freecodecamp/videos')
+
+# scrolls screen to load more videos
+def scroll_to_bottom(driver):
+
+    old_position = 0
+    new_position = None
+
+    while new_position != old_position:
+
+        old_position = driver.execute_script(
+                ("return (window.pageYOffset !== undefined) ?"
+                 " window.pageYOffset : (document.documentElement ||"
+                 " document.body.parentNode || document.body);"))
+
+        time.sleep(1)
+        driver.execute_script((
+                "var scrollingElement = (document.scrollingElement ||"
+                " document.body);scrollingElement.scrollTop ="
+                " scrollingElement.scrollHeight;"))
+
+        new_position = driver.execute_script(
+                ("return (window.pageYOffset !== undefined) ?"
+                 " window.pageYOffset : (document.documentElement ||"
+                 " document.body.parentNode || document.body);"))
+
+scroll_to_bottom(driver)
+
+html = driver.page_source
+driver.close()
+
+# find video titles
 soup = BeautifulSoup(html, 'lxml')
-details = soup.find('a', class_="reference external").text
-print(details)
+details = soup.find_all('a', id = 'video-title-link')
+details.pop()
+
+# write video titles to file
+with open('titles.txt', 'wb') as f:
+    for x in details:
+        f.write(str(x.get('title')+ '\n').encode())
